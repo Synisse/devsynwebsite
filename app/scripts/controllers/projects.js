02747991a2495de93140a38c0777b2c9f8fa4318
Synisse <stefan-krueger@hotmail.de>
2015-01-24 06:30:13 +0100

    Project data placed into seperate JSON

--- app/scripts/controllers/projects.js
+++ app/scripts/controllers/projects.js
@@ -39,153 +39,24 @@ angular.module('devsynProject')
 
     //Current placeholder for project data
     //Pulled from backend in the future
-    $scope.projects = [
-    	{
-        pid:'1',
-        name: 'OpenGL-Game',
-        technology:['C++','VS2012', 'OpenGL', 'Glut', 'Landscape Generation', 'Fractal Objects', 'Lighting'],
-        description: 'A basic OpenGL Game.',
-        image: 'http://i.imgur.com/JSwz5Az.png',
-        percent: 100,
-        youtube: 'http://www.youtube.com/embed/p3XU0tvEy70',
-        screenshots: [],
-        longdescription: 'This game was programmed from scratch with C++ and the OpenGL Utility Toolkit. During '+
-        'this project I dealt with topics like multiple viewports, ambient lighting, generated landscapes, object selections, fractal objects and '+
-        'the performance of the game.',
-        sourcecode: 'https://github.com/Synisse/OpenGLGame'
-      },
-    	{
-        pid:'2',
-        name: 'Ambient Intelligence',
-        technology:['C#', 'JavaEE', 'Hibernate', 'Wicket', 'Android', 'Machine Learning', 'Face Recognition', 'Haar Classifier', 'OpenCV'],
-        description: 'An ambient intelligence system to improve the life of a student.',
-        image: 'http://i.imgur.com/dockT8c.jpg',
-        percent: 100,
-        youtube: 'http://www.youtube.com/embed/ubRLwzv8CtI',
-        screenshots: [],
-        longdescription: 'During my minor in Ambient Intelligence I was working in a team of five people on a project that should simplify the life '+
-          'of a student at our university. This was a challenging project since it involved multiple topics such as face recognition, machine learning, mobile devices.',
-        sourcecode: ''
-      },
-      {
-        pid:'3',
-        name: 'Neural Network',
-        technology:['C#', 'WPF', 'XAML', 'Hebbian learning rule'],
-        description: 'A Neural Network to classify the color of pixels in a 100x100 Grid.',
-        image: 'http://i.imgur.com/yhPE3h6.png',
-        percent: 100,
-        youtube: '',
-        screenshots: [],
-        longdescription: 'Implementation of a neural network with an adjustable amount of hidden layers and neurons.',
-        sourcecode: ''
-      },
-      {
-        pid:'4',
-        name: 'Q-Learning',
-        technology:['C#', 'WPF', 'XAML'],
-        description: 'Implementation of the Q-Learning algorithm to find the quickest path through randomizable maze.',
-        image: 'http://i.imgur.com/V2Vsftv.png',
-        percent: 100,
-        youtube: '',
-        screenshots: [],
-        longdescription: '',
-        sourcecode: ''
-      },
-      {
-        pid:'5',
-        name: 'Face Recognition',
-        technology:['C++', 'OpenCV', 'Java', 'Android', 'Eigenfaces'],
-        description: 'A Face Recognition implementation with openCV.',
-        image: 'http://i.imgur.com/eqswyux.png',
-        percent: 100,
-        youtube: '',
-        screenshots: [],
-        longdescription: '',
-        sourcecode: ''
-      },
-      {
-        pid:'6',
-        name: 'Action Paintball',
-        technology:['Unity3D', 'C#', 'JavaScript', 'Blender', 'Cinema4D'],
-        description: 'A dynamic QuakeIII-styled paintball game. ',
-        image: 'http://i.imgur.com/2Xef5by.png',
-        percent: 30,
-        youtube: '',
-        screenshots: ['http://i.imgur.com/2KKlh46.png'],
-        longdescription: 'A dynamic QuakeIII-styled paintball game built with Unity 3D. You can '+
-          'test the current state of the game right <a target="_blank" href="http://devsyn.de/UnityTest/NetworkBuilds/webbuild0123.html">'+
-          'here </a>.',
-        sourcecode: ''
-      },
-      // {
-      //   pid:'7',
-      //   name: 'Action Sudoku',
-      //   technology:['Unity3D', 'C#', 'JavaScript'],
-      //   description: 'not yet started',
-      //   image: 'http://i.imgur.com/JSwz5Az.png',
-      //   percent: 10
-      // },
-      {
-        pid:'8',
-        name: 'Hybrid App',
-        technology:['Sencha Touch', 'JavaScript', 'HTML5', 'CSS3', 'ExtJS', 'iOS', 'Android'],
-        description: 'culimatch',
-        image: 'http://i.imgur.com/bkid9ze.png',
-        percent: 80,
-        youtube: '',
-        screenshots: [],
-        longdescription: '',
-        sourcecode: ''
-      },
-      {
-        pid:'9',
-        name: 'Forex Clock',
-        technology:['AngularJS', 'JavaScript', 'PHP', 'Ionic', 'HTML5', 'CSS3', 'Cordova', 'iOS', 'Android', 'Chrome Extensions'],
-        description: 'forex4noobs',
-        image: 'http://i.imgur.com/gawtIAC.png',
-        percent: 100,
-        youtube: '',
-        screenshots: [],
-        longdescription: '',
-        sourcecode: ''
-      },
-      {
-        pid:'10',
-        name: 'WCF Game',
-        technology:['C#', 'WCF', 'WPF', 'XAML'],
-        description: 'Implementation of the Rock-Paper-Scissors-Lizard-Spock Game.',
-        image: 'http://i.imgur.com/sLcyzlx.png',
-        percent: 100,
-        youtube: '',
-        screenshots: [],
-        longdescription: '',
-        sourcecode: ''
-      },
-      {
-        pid:'11',
-        name: 'ExamDroid',
-        technology:['XML', 'MathML', 'Java', 'Android'],
-        description: 'Android App to learn some basic Math.',
-        image: 'http://i.imgur.com/pisE9OW.png',
-        percent: 70,
-        youtube: '',
-        screenshots: [],
-        longdescription: '',
-        sourcecode: ''
-      },
-      {
-        pid:'11',
-        name: 'Devsyn Website',
-        technology:['MongoDB', 'Express', 'AngularJS', 'NodeJS', 'HTML5', 'CSS3', 'Yeoman', 'Grunt', 'Bower', 'Sass'],
-        description: 'this.website',
-        image: 'http://i.imgur.com/Xpq8Scm.png',
-        percent: 50,
-        youtube: '',
-        screenshots: [],
-        longdescription: '',
-        sourcecode: 'https://github.com/Synisse/devsynwebsite'
-      }
-    ];
+    $scope.projects = '';
+
+    $http.get('/data/projects.json').
+      success(function(data) {
+        $scope.projects = data;
+
+        $scope.projects.range = function() {
+          var range = [];
+          for( var i = 0; i < $scope.projects.length; i = i + 3 ){
+            range.push(i);
+          }
+          return range;
+        };
+
+      })
+      .error(function(){
+        console.log('project json nicht gelesen');
+      });
 
     $scope.projectstwo = '';
 
@@ -235,13 +106,7 @@ angular.module('devsynProject')
       }
     };
 
-    $scope.projects.range = function() {
-      var range = [];
-      for( var i = 0; i < $scope.projects.length; i = i + 3 ){
-          range.push(i);
-      }
-      return range;
-    };
+
 
   }]);
 
